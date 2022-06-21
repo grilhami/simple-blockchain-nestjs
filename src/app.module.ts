ff464a213bbaf82393d226e0776ee22abe05dfe7
grilhami <gilangrilhami@hotmail.co.uk>
2022-06-21 22:33:49 +0700

    Blockchain module, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlockchainController } from './blockchain/blockchain.controller';
+import { BlockchainModule } from './blockchain/blockchain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [BlockchainModule],
+  controllers: [AppController, BlockchainController],
   providers: [AppService],
 })
 export class AppModule {}
