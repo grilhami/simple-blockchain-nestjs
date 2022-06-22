36b5eb44ef95bbe87def8d5d2432517cdd5ecde7
grilhami <gilangrilhami@hotmail.co.uk>
2022-06-22 14:47:47 +0700

    DTO for blocks
    - BlockDTO for contructing the block
    - SubmitStarDTO for start data being sent
    - OwnershipDTO for when user request ownership

---
+++ src/blockchain/dto/block.dto.ts
@@ -0,0 +1,40 @@
+import { createHash } from "crypto";
+
+export class BlockDTO {
+
+    // Hash of the block
+    hash:string = ""
+
+    // Block Height (consecutive number of each block)
+    height:number = 0;
+    
+    // Will contain the transactions stored in the block, by default it will encode the data
+    body:string = "";
+
+    // Timestamp for the Block creation
+    time:number = 0;
+    
+    // Reference to the previous Block Hash
+    previousBlockHash:string = "";
+}
+
+export class SubmitStarDTO {
+
+    // User wallet address
+    address:string;
+
+    // Star Message
+    message:string;
+
+    // Signature
+    signature:string;
+
+    // Star Name
+    star:string;
+}
+
+
+export class OwnershipDTO {
+    // User wallet address
+    address:string;
+}
