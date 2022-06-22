f07df5489e800e7ecf35398b17eb23be99404f06
grilhami <gilangrilhami@hotmail.co.uk>
2022-06-22 21:38:19 +0700

    Implement _addBlock method in BlockchainService

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { createHash } from 'crypto';
+import { BlockService } from 'src/block/block.service';
 import { BlockDTO } from './dto';
 
 
@@ -16,11 +18,39 @@ export class BlockchainService {
     }
 
 
+    async initializeChain():Promise<void> {
+        if( this.height === -1){
+            const block: BlockDTO = {
+                body: "Genesis Block",
+                hash: '',
+                height: 0,
+                time: 0,
+                previousBlockHash: ''
+            }
+            await this._addBlock(block);
+        }
+    }
 
-    async initializeChain() {
-        // if( this.height === -1){
-        //     let block = new BlockClass.Block({data: 'Genesis Block'});
-        //     await this._addBlock(block);
-        // }
+    _addBlock(block: BlockDTO):Promise<void> {
+        const self = this;
+        return new Promise(async (resolve, reject) => {
+
+            if (self.chain.length > 0) {
+                block.height = self.chain.length ;
+                block.previousBlockHash = self.chain[self.chain.length - 1].hash;
+            }
+
+            block.body = Buffer.from(JSON.stringify(block.body)).toString("hex");
+            block.time = new Date().getTime();
+
+            const blockObj: BlockService = new BlockService(block)
+            await blockObj.validate()
+
+            const blockStr: string = JSON.stringify(block)
+            block.hash = createHash("sha256").update(blockStr).digest("hex")
+
+            self.chain.push(block)
+        });
+        
     }
 }
